37f50fa5c16f6393cfe9840143501214aea0d95a
mahabubsaki <96987600+mahabubsaki@users.noreply.github.com>
2022-04-29 14:43:43 +0600

    connected to database and secured user password with env file

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const port = process.env.PORT || 5000
 //middleware
 app.use(cors())
 app.use(express.json())
+//Database Configuration
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@assingmentcluster.jo7ra.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 //root directory of server and for checking my server
 app.get('/', (req, res) => {
     res.send('Hello Assingment 11 :)')
@@ -14,3 +19,13 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`My node server running on port ${port}`)
 })
+
+async function run() {
+    try {
+        await client.connect();
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir)
