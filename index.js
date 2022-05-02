b8ccd87bd64c47ee0821281a07ac9a5e0f4c3f0a
mahabubsaki <96987600+mahabubsaki@users.noreply.github.com>
2022-05-02 13:50:27 +0600

    created a tokenverify function separatly

--- index.js
+++ index.js
@@ -20,7 +20,20 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`My node server running on port ${port}`)
 })
-
+const tokenVerify = async (req, res, next) => {
+    const authHeader = req.headers.authorization
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthorized Access' })
+    }
+    const token = authHeader.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden Access' })
+        }
+        req.decoded = decoded
+        next()
+    })
+}
 async function run() {
     try {
         await client.connect();
